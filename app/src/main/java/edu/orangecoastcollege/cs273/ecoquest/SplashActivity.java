0c8a94bea6df315d7b440c67d22b257fa804ecff
brendan <alber123@mail.chapman.edu>
2017-11-12 15:27:25 -0800

    added splash screen !

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/ecoquest/SplashActivity.java
@@ -0,0 +1,28 @@
+package edu.orangecoastcollege.cs273.ecoquest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                finish();
+                Intent intent = new Intent(SplashActivity.this, DashboardActivity.class);
+                startActivity(intent);
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(task, 3000);
+    }
+}
