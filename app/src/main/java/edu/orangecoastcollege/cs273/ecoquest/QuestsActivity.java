acce20316fe60fe61661434fc980875d388a48ad
brendan <alber123@mail.chapman.edu>
2017-11-30 14:47:58 -0800

    added some features to QuestsActivity

--- app/src/main/java/edu/orangecoastcollege/cs273/ecoquest/QuestsActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/ecoquest/QuestsActivity.java
@@ -49,11 +49,6 @@ public class QuestsActivity extends AppCompatActivity {
 
     private List<Quest> filterParkQuests()
     {
-        //int[] questTypes;
-        //for (Quest quest : mAllQuestsList) {
-           // questTypes = quest.getQuestTypes();
-           // if ( questTypes QuestType.PARK)
-        //}
         return null;
     }
 
